aeec1fd05b09e9f0e8dc2be28a556ecbd897986b
sugarbody <sugarheadbody@gmail.com>
2025-09-09 13:54:56 -0500

    change data

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,7 +1,7 @@
 export const socialsData = {
     github: 'https://github.com/undertheme',
     facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in/moiz-ahmed-192268382/',
+    linkedIn: 'https://www.linkedin.com/in/moiz-ahmed-ab3897383/',
     instagram: 'https://www.instagram.com/',
     twitter: 'https://twitter.com/',
     youtube: 'https://youtube.com/'
