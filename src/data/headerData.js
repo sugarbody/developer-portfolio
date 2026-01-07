f9674bff565e7fcd73accdc7cf750bc667a98295
sugarbody <sugarheadbody@gmail.com>
2026-01-07 13:15:27 -0600

    remove photo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
     name: 'Moiz Ahmed',
     title: "Senior ML/MLOps Engineer",
     desciption:"A highly skilled ML/MLOps Engineer with over 10 years of experience in designing, deploying, and managing scalable AI systems across multi-cloud platforms, specializing in generative AI, automated ML pipelines, and ensuring system reliability and security to drive impactful business outcomes.",
-    image: photo,
+    // image: photo,
     resumePdf: resume
 }
